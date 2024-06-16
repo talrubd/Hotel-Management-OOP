aa72620866f0fa0f63989b8761b5203f048f8946
Carl Bergado <115414567+Carts1024@users.noreply.github.com>
2024-06-17 00:11:47 +0800

    Add files via upload

---
+++ Db Crud/Db Crud/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Db_Crud
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
